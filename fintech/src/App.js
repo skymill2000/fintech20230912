3b833d873df6942d97afcf373c5122036355268d
유관우 <skymill2000@gmail.com>
2023-09-12 16:48:25 +0900

    Lecture(React): React-router-dom 추가 및 app.js 파일 변경  npm install react-router-dom

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,22 +1,13 @@
-import logo from "./logo.svg";
-import Welcome from "./components/Welcome";
-import "./App.css";
-import StateComponent from "./components/StateComponent";
-import EventComponent from "./components/EventComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListComponent from "./components/ListComponent";
-import StyledComponent from "./components/StyledComponent";
-import HeaderComponent from "./components/HeaderComponent";
 
 function App() {
   return (
-    <div className="App">
-      <HeaderComponent></HeaderComponent>
-      <Welcome username="유관우1" age={35}></Welcome>
-      {/* <StateComponent></StateComponent> */}
-      <EventComponent></EventComponent>
-      <ListComponent></ListComponent>
-      <StyledComponent></StyledComponent>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListComponent></ListComponent>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
