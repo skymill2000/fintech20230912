c780cc889af12ebbfaa4588584c023449b9021ce
유관우 <skymill2000@gmail.com>
2023-09-12 14:50:21 +0900

    Lecture(리액트 프로젝트 적용): 리액트 시작 1, npm install create-react-app -g //모듈 설치 2. npx create-react-app fintech //프로젝트 적용 3. cd fintech //프로젝트 디렉토리 이동     npm start // 프로젝트 실행

---
+++ fintech/src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const Welcome = () => {
+  return (
+    <>
+      <p>안녕하세요!</p>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome></Welcome>
+    </div>
+  );
+}
+
+export default App;
