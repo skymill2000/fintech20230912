388c43eada3b57fcb70c4d1f2597e5dd170d298f
유관우 <skymill2000@gmail.com>
2023-09-12 15:44:38 +0900

    Lecture(React): 이벤트 바인딩

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import Welcome from "./components/Welcome";
 import "./App.css";
 import StateComponent from "./components/StateComponent";
+import EventComponent from "./components/EventComponent";
 
 function App() {
   return (
     <div className="App">
       <Welcome username="유관우1" age={35}></Welcome>
       <StateComponent></StateComponent>
+      <EventComponent></EventComponent>
     </div>
   );
 }
