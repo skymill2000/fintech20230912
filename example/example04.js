9b8fca63ee8a0d1b44dfc7f4109ed0370204f20f
유관우 <skymill2000@gmail.com>
2023-09-12 13:23:53 +0900

    Lecture(Javascript Basic): 배열의 활용과 반복문 map, for...of

--- example/example04.js
+++ example/example04.js
@@ -10,4 +10,22 @@ let car = {
 };
 
 let array = ["sonata", 1234, car];
-console.log(array[1]);
+
+const points = [40, 100, 1, 5, 25, 10];
+
+// console.log(points[0]);
+// console.log(points[1]);
+// console.log(points[2]);
+//...
+
+for (let i = 0; i < points.length; i++) {
+  console.log(points[i]);
+}
+console.log("----------");
+for (point of points) {
+  console.log(point);
+}
+console.log("----------");
+points.map((point) => {
+  console.log(point);
+});
