def2347cb86717f1dc937f9e4d6b963b55a66a6f
유관우 <skymill2000@gmail.com>
2023-09-12 15:30:48 +0900

    Lecture(React): UseState 를 활용한 값 변경

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import Welcome from "./components/Welcome";
 import "./App.css";
+import StateComponent from "./components/StateComponent";
 
 function App() {
   return (
     <div className="App">
       <Welcome username="유관우1" age={35}></Welcome>
-      <Welcome username="유관우2" age={35}></Welcome>
-      <Welcome username="유관우3" age={35}></Welcome>
+      <StateComponent></StateComponent>
     </div>
   );
 }
