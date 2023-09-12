63c9adbd32f8126796feecebb1b2e1a54d1e8d94
유관우 <skymill2000@gmail.com>
2023-09-12 13:36:15 +0900

    Lecture(Javascript): 자바스크립트 템플릿 리터럴 샘플 추가

---
+++ example/example05.js
@@ -0,0 +1,13 @@
+let car = {
+  name: "소나타",
+  ph: 300,
+  maker: "현대",
+};
+
+let carName = car.name;
+let carPh = car.ph;
+let carMaker = car.maker;
+
+const { name, ph } = car;
+
+console.log(name, carName);
---
+++ example/example06.js
@@ -0,0 +1,7 @@
+let username = "홍길동";
+let title = "안녕하세요";
+let greeting = "35세 " + title + username + "님" + "만나서 반갑습니다.";
+let template = `${username}님 ${title} 반갑습니다`;
+
+console.log(greeting);
+console.log(template);
