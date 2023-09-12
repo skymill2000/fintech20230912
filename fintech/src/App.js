b60efa965c70be4a7021389d8f5cb4d40a5d2164
유관우 <skymill2000@gmail.com>
2023-09-12 16:07:36 +0900

    Lecture(React): ListComponent 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,13 +3,15 @@ import Welcome from "./components/Welcome";
 import "./App.css";
 import StateComponent from "./components/StateComponent";
 import EventComponent from "./components/EventComponent";
+import ListComponent from "./components/ListComponent";
 
 function App() {
   return (
     <div className="App">
       <Welcome username="유관우1" age={35}></Welcome>
-      <StateComponent></StateComponent>
+      {/* <StateComponent></StateComponent> */}
       <EventComponent></EventComponent>
+      <ListComponent></ListComponent>
     </div>
   );
 }
