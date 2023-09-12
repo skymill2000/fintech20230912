10259b6e5528e356a3be7e86146a4651a3c1ace6
유관우 <skymill2000@gmail.com>
2023-09-12 15:14:07 +0900

    Lecture(React): 컴포넌트와 프롭스

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const Welcome = () => {
+const Welcome = ({ username, age }) => {
   return (
     <>
-      <p>안녕하세요!</p>
+      <p>
+        {username}님 {age}세 안녕하세요!
+      </p>
     </>
   );
 };
@@ -12,7 +14,9 @@ const Welcome = () => {
 function App() {
   return (
     <div className="App">
-      <Welcome></Welcome>
+      <Welcome username="유관우1" age={35}></Welcome>
+      <Welcome username="유관우2" age={35}></Welcome>
+      <Welcome username="유관우3" age={35}></Welcome>
     </div>
   );
 }
