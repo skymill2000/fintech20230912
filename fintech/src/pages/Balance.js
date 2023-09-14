e15a6c0d37a003f1b969214313a7799a033a7247
유관우 <skymill2000@gmail.com>
2023-09-14 13:16:10 +0900

    Lecture(Project): 거래내역 조회  - 잔액조회 디자인 적용

--- fintech/src/pages/Balance.js
+++ fintech/src/pages/Balance.js
@@ -3,6 +3,8 @@ import HeaderComponent from "../components/HeaderComponent";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import queryString from "query-string";
+import BalanceCard from "../components/balance/BalanceCard";
+import TransactionList from "../components/balance/TransactionList";
 
 const Balance = () => {
   const queryParams = useLocation().search;
@@ -10,8 +12,10 @@ const Balance = () => {
   const fintechNo = parsed.fintechUseNo;
 
   const [balance, setBalance] = useState("0");
+  const [trasactionList, setTrasactionList] = useState([]);
   useEffect(() => {
     getBalance();
+    getTransactionList();
   }, []);
   const getBalance = () => {
     const accessToken = localStorage.getItem("accessToken");
@@ -31,7 +35,33 @@ const Balance = () => {
 
     axios(requestOption).then((response) => {
       console.log(response);
-      setBalance(response.data.balance_amt);
+      setBalance(response.data);
+    });
+  };
+
+  const getTransactionList = () => {
+    const accessToken = localStorage.getItem("accessToken");
+    let requestOption = {
+      url: "/v2.0/account/transaction_list/fin_num",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        bank_tran_id: genTrasId(),
+        fintech_use_num: fintechNo,
+        inquiry_type: "A",
+        inquiry_base: "D",
+        from_date: "20230912",
+        to_date: "20230914",
+        sort_order: "A",
+        tran_dtime: "20230914103600",
+      },
+    };
+
+    axios(requestOption).then(({ data }) => {
+      setTrasactionList(data.res_list);
     });
   };
 
@@ -51,7 +81,12 @@ const Balance = () => {
   return (
     <div>
       <HeaderComponent title={"잔액조회 / 거래내역"}></HeaderComponent>
-      {balance}
+      <BalanceCard
+        bankName={balance.bank_name}
+        fintechNo={fintechNo}
+        balance={balance.balance_amt}
+      ></BalanceCard>
+      <TransactionList transactionList={trasactionList}></TransactionList>
     </div>
   );
 };
