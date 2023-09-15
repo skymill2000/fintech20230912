a243153e0b7c3778d731aaf8bd58cf6b9b69bf45
유관우 <skymill2000@gmail.com>
2023-09-15 11:30:28 +0900

    Lecture(암호화): AES 암호화 로직 작성

--- example/Enc.js
+++ example/Enc.js
@@ -8,3 +8,19 @@ const sha256Enc = (plainText, key) => {
     .digest("base64");
   return hash;
 };
+
+const AESEncrypt = (plainTxt) => {
+  const algorithm = "aes-256-cbc";
+  const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
+  const iv = "dwUgwmwOndnJOmOX";
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(plainTxt, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  console.log(encrypted);
+  return encrypted;
+};
+const AESDecrypt = (plainTxt) => {
+  const algorithm = "aes-256-cbc";
+  const key = "H3uVnnOnNIXXsH63CsgswUsmICSCdVsn";
+  const iv = "dwUgwmwOndnJOmOX";
+};
